2c54aa6c687a1c571c8417b3acd942fde9e65279
Shinhu Park <bonnydog51@gmail.com>
2025-09-11 02:31:24 +0900

    Added classes for DX Kuma item

---
+++ src/main/java/com/example/item/ModItems.java
@@ -0,0 +1,30 @@
+package com.example.item;
+
+import com.example.TemplateMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item DX_KUMA = registerItem("dx_kuma", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TemplateMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        TemplateMod.LOGGER.info("Registering Mod items for" + TemplateMod.MOD_ID);
+
+        //This adds item to "ingredients" tab of creative mode
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(DX_KUMA);
+        });
+
+    }
+
+}

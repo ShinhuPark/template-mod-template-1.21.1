4fb5cf6f90655c152edc62c8ca6d51648e6ce078
Shinhu Park <bonnydog51@gmail.com>
2025-09-11 02:03:44 +0900

    Hi. I'm Niko

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class TemplateMod implements ModInitializer {
 	public static final String MOD_ID = "template-mod";
-
+    // Give me some pancakeeeees
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
